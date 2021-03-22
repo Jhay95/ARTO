be6730703b4d812b2bdd94e5c3a2d28f019ee98d
judith <missjay2805@gmail.com>
2021-03-22 19:33:49 +0000

    footer and contact page adjusted

--- Private/views/includes/footer.php
+++ Private/views/includes/footer.php
@@ -2,29 +2,35 @@
 <footer class="page-footer mt-auto">
     <div class="container text-center">
         <div class="row g-3">
-            <div class="col-sm-3">
+            <div class="col-sm-2">
                 <div id="copy">
                     <p>ARTO &copy; <?php echo date('Y'); ?></p>
                 </div>
             </div>
 
-            <div class="col-sm-6">
+            <div class="col-sm-5">
                 <div id="footernav">
-                    <a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages">Home</a>
-                    <a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages/register">Tell a Story</a>
-                    <a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages/stories">Stories</a>
-                    <a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages/contact">Contact</a>
+                    <p><a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages">Home</a></p>
+                    <p><a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages/register">Tell a Story</a></p>
+                    <p><a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages/stories">Stories</a></p>
+                    <p><a class="nav-item nav-link" href="<?php echo URL_ROOT; ?>pages/contact">Contact</a></p>
                 </div>
             </div>
 
-            <div class="col-sm-3" >
+            <div class="col-sm-3">
+                <div id="policy">
+                    <p><a class="nav-item nav-link" href="#">T & C</a></p>
+                    <p><a class="nav-item nav-link" href="#">Privacy Policy</a></p>
+                </div>
+            </div>
+
+            <div class="col-sm-2" >
                 <div id="media-links">
-                    <a href="#" class="fa fa-facebook"></a>
-                    <a href="#" class="fa fa-twitter"></a>
-                    <a href="#" class="fa fa-youtube"></a>
+                    <p><a href="#" class="fa fa-facebook"></a></p>
+                    <p><a href="#" class="fa fa-twitter"></a></p>
+                    <p><a href="#" class="fa fa-youtube"></a></p>
                 </div>
             </div>
-
         </div>
     </div>
 </footer>
