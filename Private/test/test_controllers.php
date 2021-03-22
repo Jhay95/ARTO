47fc22b0530d4575ba91bdd0ca19046e3dbaa6df
judith <missjay2805@gmail.com>
2021-03-22 19:33:25 +0000

    controller test re-evaluated

--- Private/test/test_controllers.php
+++ Private/test/test_controllers.php
@@ -1,40 +1,53 @@
 <?php
 require_once('test.php');
 
-//1. Other methods to test [index==home==default, about,contact,stories,login]
+// Comment out each number for testing
+
+
+/*//1. Other methods to test [index==home==default, about,contact,stories,login]
 $page_header = testControllers('GET','pages','about');
 assert(strpos($page_header[0], '200') == true);
+*/
 
-
-//2.  Test Registration method works with unregistered email
+/*//2.  Test Registration method works with unregistered email
 $reg_form = [
-    'fname'=> 'Silva',
-    'lname'=> 'Coker',
+    'fname'=> '',
+    'lname'=> '',
     'city'=> 'Nottington',
     'state'=> 'London',
-    'email'=> 'silva@gmail.com',
-    'password'=> 'Silv537$3#$',
+    'email'=> '@gmail.com',
+    'password'=> '#$',
 ];
 $reg_result = testControllers('POST', 'writers', 'register', '', $reg_form,);
 assert(!empty($reg_result['location'][0]) === true);
 assert(strpos($reg_result[0], '302') == true);
-assert(strpos($reg_result['location'][0], 'writers/index') == true);
+assert(strpos($reg_result['location'][0], 'writers/index') == true);*/
 
 
 
-//3. Test Login works with right credentials
+/*//3. Test Login works with right credentials
 $login_form = [
-    'email'=> 'silva@gmail.com',
-    'password'=> 'Silv537$3#$',
+    'email'=> '@gmail.com',
+    'password'=> '#$',
 ];
 
 $login_result = testControllers('POST', 'writers', 'login', '' ,$login_form);
 assert(!empty($login_result['location'][0]) === true);
 assert(strpos($login_result[0], '302') == true);
-assert(strpos($login_result['location'][0], 'writers/index') == true);
+assert(strpos($login_result['location'][0], 'writers/index') == true);*/
+
+/**
+ * Testing Login Session
+ */
+
+/*//1. Accessing a profile page when not in session and not logged in
+$get_profile = testControllers('GET', 'writers', 'index', 16);
+assert(!empty($get_profile['location']) === true);
+assert(strpos($get_profile[0], '302') == true);
+assert(strpos($get_profile['location'], 'pages/error') == true);*/
 
 
-//4. Test Editing profile with registered email
+/*//2. Editing profile with registered email
 $update_form = [
     'fname'=> '',
     'lname'=> '',
@@ -43,41 +56,32 @@ $update_form = [
     'city' => '',
     'state' => ''
 ];
-$update_result = testControllers('POST', 'writers', 'edit', 12 ,$update_form);
-assert(!empty($update_result['location'][0]) === true);
+$update_result = testControllers('POST', 'writers', 'edit', 17 ,$update_form);
+var_dump($update_result);
+assert(!empty($update_result['location']) === true);
 assert(strpos($update_result[0], '302') == true);
-assert(strpos($update_result['location'][0], 'writers/index') == true);
+assert(strpos($update_result['location'], 'pages/error') == true);*/
 
 
-
-//5. Test Deleting profile with registered email
-$deleted_result = testControllers('GET', 'writers','delete', 12);
+/*//3. Deleting profile with registered email
+$deleted_result = testControllers('GET', 'writers','delete', 10);
+var_dump($deleted_result);
 assert(!empty($deleted_result['location']) === true);
 assert(strpos($deleted_result[0], '302') == true);
-assert(strpos($deleted_result['location'], 'pages/login') == true);
+assert(strpos($deleted_result['location'], 'pages/error') == true);
+//Failed - action completed and redirected to login page*/
 
 
-
-
-//6. Test - Accessing a profile page when not in session and not logged in
-$get_profile = testControllers('GET', 'writers', 'index', 16);
-assert(!empty($get_profile['location']) === true);
-assert(strpos($get_profile[0], '302') == true);
-assert(strpos($get_profile['location'], 'pages/index') == true);
-
-
-
-//7. Test - Creating a new story by a writer
+/*//4. Creating a new story by a writer
 $story = [
     'title' => '',
-    'body' => '',
+    'body' => 'Lorem ipsum',
     'category' => 'Tradition',
     'tag' => 'home',
     'location' => 'Chrisville',
 ];
 
 $new_story = testControllers('POST', 'stories', 'new', 16, $story);
-assert(!empty($new_story['location'][0]) === true);
+assert(!empty($new_story['location']) === true);
 assert(strpos($new_story[0], '302') == true);
-assert(strpos($new_story['location'][0], 'writers/index') == true);
-
+assert(strpos($new_story['location'], 'pages/error') == true);*/
